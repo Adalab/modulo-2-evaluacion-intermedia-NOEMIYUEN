a74baffdf25cbd982b0ebac20c6abe7c5044f39e
noemiyuen <noemiyuen@gmail.com>
2022-10-31 11:04:39 +0100

    Start Bonus

--- js/main.js
+++ js/main.js
@@ -7,35 +7,19 @@ const userElement = document.querySelector ('.js-user');
 const computerElement = document.querySelector('.js-computer');
 
 
-/* 
-Raza 1: Pelosos con fuerza (1),
-Raza 2: Sureños buenos con fuerza (2),
-Raza 3: Enanos con fuerza (3),
-Raza 4: Númenóreanos con fuerza (4),
-Raza 5: Elfos con fuerza (5).
-Razas malvadas:
-Raza 1: Sureños malos con fuerza (2),
-Raza 2: Orcos con fuerza (2),
-Raza 3: Goblins con fuerza (2),
-Raza 4: Huargos con fuerza (3),
-Raza 5: Trolls con fuerza(5)
+const lose  = '¡Ha ganado el Ejército del Mal! Vuelve a Intentarlo!';
+const win = '¡Ha ganado el Ejército del Bien! Enhorabuena!';
+const tie = '¡Empate!'
 
- */
 //función nºaleatorio, redondea hacia arriba (parte de 1)
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
     }
 
-//Incluir carter
+//Incluir Cartel
 function renderResult (resultText) {
   resultElement.innerHTML=resultText;
 }
-
-const lose  = '¡Ha ganado el Ejército del Mal! Vuelve a Intentarlo!';
-const win = '¡Ha ganado el Ejército del Bien! Enhorabuena!';
-const tie = '¡Empate!'
-/* renderResult('Ha ganado el Ejército del Mal! Vuelve a Intentarlo.') */
-
 
 //función batalla
 function battle () {
@@ -44,11 +28,13 @@ function battle () {
     console.log (random);
     if (parseInt(selectElement.value)===1) {
       renderResult(lose);
+      sauronPoints++
     }
 
 
     if (parseInt(selectElement.value)===2 && random >3) {
       renderResult(lose);
+      sauronPoints++
       } 
     if (parseInt(selectElement.value)===2 && random <=3) {
       renderResult(tie);
@@ -57,24 +43,29 @@ function battle () {
 
    if (parseInt(selectElement.value)===3 && random <4) {
       renderResult(win);
+      userPoints++
     } 
     if (parseInt(selectElement.value)===3 && random ===4) {
       renderResult(tie);
     }
     if (parseInt(selectElement.value)===3 && random >4) {
       renderResult(lose);
+      sauronPoints++
     } 
+ 
 
-    
    if (parseInt(selectElement.value)===4 && random <5) {
     renderResult(win);
+    userPoints++
     } 
   if (parseInt(selectElement.value)===4 && random ===5) {
     renderResult(lose);
+    sauronPoints++
     } 
 
     if (parseInt(selectElement.value)===5 && random <5) {
       renderResult(win);
+      userPoints++
       } 
     if (parseInt(selectElement.value)===5 && random ===5) {
       renderResult(tie);
@@ -83,13 +74,32 @@ function battle () {
 
 
 
+
+
 //función manejadora
 
 function handleClick(event) {
   event.preventDefault();
     battle ();
-
+    games++;
+    console.log(userPoints+'points user');
+    console.log(sauronPoints+'points sauron');
+    
 }
     
 //funcion evento
 btnElement.addEventListener('click', (handleClick));
+
+//bonus
+let games = 0;
+let userPoints = 0;
+let sauronPoints = 0;
+
+
+function userScore () {
+  userElement.innerHTML = `Puntos del bien ${userPoints}`;
+}
+function sauronScore () {
+  computerElement.innerHTML = `Puntos de Sauron ${sauronPoints}`;
+}
+
